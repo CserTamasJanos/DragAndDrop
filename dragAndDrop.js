debc7dc6eff3f746be4ad00ce308d6890a59e7fa
CserTamasJanos <70898590+CserTamasJanos@users.noreply.github.com>
2021-03-26 12:54:19 +0100

    ToCheck

---
+++ dragAndDrop.js
@@ -0,0 +1,41 @@
+var list = document.getElementById('list')
+var dragging, draggedOver;
+var daniText = ["a","szöveg","sorba","Ez","most","állítva!","van",];
+
+function renderItems(data)
+{
+    for(let i = 0; i < daniText.length; i++)
+    {
+        var node = document.createElement("li");    
+        node.draggable = true
+        node.style.backgroundColor = '#b3ecff';
+        node.addEventListener('drag', setDragging);
+        node.addEventListener('dragover', setDraggedOver);
+        node.addEventListener('drop', compare);
+        node.innerText = daniText[i];
+        node.tabIndex = i;
+        list.appendChild(node);
+    }
+}
+
+function  compare()
+{
+    if(dragging.tabIndex != draggedOver.tabIndex)
+    {
+        var temp = draggedOver.innerText;
+        list.children[draggedOver.tabIndex].innerText = dragging.innerText;
+        list.children[dragging.tabIndex].innerText = temp;
+    }
+}
+
+function setDraggedOver(e){
+  e.preventDefault();
+  draggedOver = e.target;
+}
+
+function setDragging(e){
+    dragging = e.target;
+}
+
+renderItems();
+
